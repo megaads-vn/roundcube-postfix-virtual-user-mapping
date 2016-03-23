3c78809efb7063113d41fe51ba0f1e749350ac64
GDR! <gdr@gdr.name>
2016-03-23 20:41:27 +0100

    Readme

--- README.md
+++ README.md
@@ -1,68 +1,11 @@
-# Roundcube-Plugin-Virtual-User-Mapping
-Virtual User mapping multiple user logins for one account
-  
+# Roundcube Postfix Virtual User Mapping
 
 Add in your config file:
 
 ```php
-$config['virtualUserMapping'] = array(
-  'REAL DOMAIN 1' => array(
-    'REAL ACCOUNT 1' => array(
-      'pass' => 'REAL ACCOUNT PASS',
-        'users' => array(
-          'VIRTUAL USER NAME 1' => 'VIRTUAL USER PASS 1',
-          'VIRTUAL USER NAME 2' => 'VIRTUAL USER PASS 2',
-            ...
-          )
-        ),
-    'REAL ACCOUNT 2' => array(
-      'pass' => 'REAL ACCOUNT 2 PASS',
-      'users' => array(
-        'VIRTUAL USER NAME 3' => 'VIRTUAL USER PASS 3 '
-          ...
-        )
-      ),
-      ...
-    )
- );
+$config['postfixVirtualMapPath'] = "/etc/postfix/virtual";
  ```
 
-Now you can login with "VIRTUAL USER NAME 1#REAL DOMAIN 1".
-For example:
+Now you can login with "user@domain.com" instead of "user", as defined in virtual users table in Postfix.
 
-```php
-$config['virtualUserMapping'] = array(
-  'example123.com' => array(
-    'sales.web99' => array(
-      'pass' => 'imapSecretForRealAccountSales',
-        'users' => array(
-          'practicant' => 'reallyLongPassword',
-          'teamleader' => 'anotherLongPassword',
-            ...
-          )
-    ),
-    'support.web99' => array(
-      'pass' => 'anotherImapSecret',
-      'users' => array(
-        'jefe' => 'extremLongPasswordWithALotOfSpecialChars '
-          ...
-        )
-      ),
-      ...
-    )
- );
- 
- With a configuration like this you have following logins:
-
-
-| User                             | Password                                  | Mapped to Real Account  |
-| -------------------------------- |-------------------------------------------| ------------------------|
-| practicant#sales@example123.com  | reallyLongPassword                        | sales.web99             |
-| teamleader#sales@example123.com  | anotherLongPassword                       | sales.web99             |
-| jefe#support@example123.com      | extremLongPasswordWithALotOfSpecialChars  | support.web99           |
-
-
-Please note:
-The "#" is used as a splitter between the virtual user name and the existing imap account name.
-
- 
+Note: This is reading from the text file, not from Postfix's optimized .db file.
---
+++ postfix_virtual_user_mapping.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * Virtual User mapping multiple user logins for one account
+ *
+ * @version 1.0
+ * @license GNU GPLv3+
+ * @author GDR!
+ * @author Pierre Arlt
+ */
+class postfix_virtual_user_mapping extends rcube_plugin
+{
+    const LOGIN_DATA_USER = 'user';
+    const LOGIN_DATA_PASSWORD = 'password';
+    const LOGIN_DATA_PASS = 'pass';
+
+    /** @var  rcmail */
+    private $app;
+
+    /** @var  string */
+    private $postfixVirtualPath = '/etc/postfix/virtual';
+
+    /** @var string  */
+    private $logName = 'postfix_virtual_user_log';
+
+
+    function init()
+    {
+        $this->app = rcmail::get_instance();
+        $this->add_hook('authenticate', array($this, 'login'));
+    }      
+
+    /**
+     * @param int $errcode
+     * @return string
+     */
+    function preg_errtxt($errcode)
+    {
+        static $errtext;
+
+        if (!isset($errtxt))
+        {
+            $errtext = array();
+            $constants = get_defined_constants(true);
+            foreach ($constants['pcre'] as $c => $n) if (preg_match('/_ERROR$/', $c)) $errtext[$n] = $c;
+        }
+
+        return array_key_exists($errcode, $errtext)? $errtext[$errcode] : NULL;
+    }
+
+    /**
+     * @param $loginData
+     * @return mixed
+     */
+    public function login($loginData)
+    {
+        if (strpos($loginData[self::LOGIN_DATA_USER], '@') !== false) {
+            $postfixVirtualPath = $this->app->config->get('postfixVirtualMapPath');
+            if($postfixVirtualPath) {
+                $this->postfixVirtualPath = $postfixVirtualPath;
+            }
+            if (!is_readable($this->postfixVirtualPath)) {
+                $this->writeLog('postfixVirtualPath ' . $this->postfixVirtualPath . ' is not readable');
+                return $loginData;
+            }
+
+            $email = $loginData[self::LOGIN_DATA_USER];
+            $password = $loginData[self::LOGIN_DATA_PASS];
+
+            $this->writeLog('Mapping email: ' . $email);
+
+            $escaped_email = preg_quote(strtolower($email));
+            $preg = "/^$escaped_email\\s+(?P<username>[a-zA-Z0-9_]+)(\\s|$)/i";
+
+            $f = fopen($this->postfixVirtualPath, "r");
+            while (($line = fgets($f, 4096)) !== false) {
+                $match = preg_match(
+                    $preg,
+                    $line,
+                    $matches
+                );
+                if ($match === false) {
+                    $this->writeLog('PREG error: ' . preg_errtxt(preg_last_error()) . ' - expression: ' . $preg);
+                }
+                if ($match) {
+                    $loginData[self::LOGIN_DATA_USER] = $matches['username'];
+                    $this->writeLog('Rewrote username ' . $email . ' with ' . $matches['username']);
+                }
+            }
+
+        return $loginData;
+    }
+
+
+    /**
+     * @param string $data
+     */
+    function writeLog($data)
+    {
+        $this->app->write_log($this->logName, $data);
+    }
+}
